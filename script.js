26e27fb1e127f4aded21a059c0b5aea7d00f9832
Carter Duong <greasybutton@gmail.com>
2018-05-02 11:32:11 -0700

    final

--- script.js
+++ script.js
@@ -22,14 +22,18 @@ $(document).ready(function(){
         ms = "000";
       }
 
-      //document.getElementById("timer").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
-      document.getElementById("timer").innerHTML = days + "d : " + hours + "h : " + ms + "ms" + "<br>" + "☁︎&emsp;☁&emsp;☁︎︎";
+      var cloudText = "<br>" + "☁︎&emsp;☁&emsp;☁︎︎";
+      cloudText = cloudText.fontcolor("#F7F9E0");
+
+      document.getElementById("timer").innerHTML = days + "d : " + hours + "h : " + ms + "ms" + cloudText;
+
 
       // If the count down is finished, write some text 
       if (distance < 0) {
         clearInterval(x);
         document.getElementById("timer").innerHTML = "0d : 0h : 0ms";
       }
+
     }, 100);
 });
 
@@ -37,9 +41,3 @@ $(window).resize(function() {
     $("div").width( $(window).width() );
     $("div").height( $(window).height() );
 });
-
-Number.prototype.pad = function(size) {
-  var s = String(this);
-  while (s.length < (size || 2)) {s = "0" + s;}
-  return s;
-}
